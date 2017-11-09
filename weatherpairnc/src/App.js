61eebc1b77b9b83666c1d0d38926a32df54f6ceb
Timbuckiii <30511045+Timbuckiii@users.noreply.github.com>
2017-11-09 18:02:34 -0500

    first commit with Chart component

--- weatherpairnc/src/App.js
+++ weatherpairnc/src/App.js
@@ -1,12 +1,15 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 import axios from 'axios';
-// import * as d3 from "d3";
+import * as d3 from "d3";
+import Chart from "./Chart"
 
 //OpenWeatherMap API Key 04555c7b75822478f6403a14d3b3a458
 
 let weatherAPI = 'http://api.openweathermap.org/data/2.5/forecast?id=6167865&units=metric&APPID=04555c7b75822478f6403a14d3b3a458';
+let weatherJSX;
+let weatherArray;
+
 class App extends Component {
   constructor() {
     super();
@@ -16,8 +19,8 @@ class App extends Component {
     }
     // Example binding
     // this.playSong = this.playSong.bind(this);
-    // this.componentWillMount = this.componentWillMount.bind(this);
-  } 
+  }
+
   componentWillMount(){
     axios.get(weatherAPI)
     .then(response => {
@@ -34,36 +37,22 @@ class App extends Component {
   }
 
   render() {
-    // let copy = Object.assign({}, this.state.weatherInfo);
-    // let copy = Array.from(this.state.weatherInfo)
-    // console.log('the copy:', copy.list);
-    //   for(let i = 0; i <= copy.list.length; i++)
-    // {
-    //   console.log(copy.list[i].main.temp)
-    // }
-    
-    
-    // let copy = Array.from(this.state.weatherInfo)
+
     if(this.state.loading === false){
-    let weatherArray = Array.from(this.state.weatherInfo.list);
-    // console.log('Weather Array ', weatherArray);
-    for(let i = 0; i < 40; i++)
-    {
-      console.log(weatherArray[i].main.temp)
+      weatherArray = Array.from(this.state.weatherInfo.list);
+      // weatherJSX = weatherArray.map((individualTemp, index) => {
+      //     return (
+      //     <div>
+      //         <h3>{individualTemp.main.temp}</h3>
+      //     </div>
+      //     )
+      // });
     }
-  }
-    // let weatherJSX = copy.map((tempz, index) => {
-    //     return (
-    //     <div>
-    //         <h3>{tempz.main.temp}</h3>
-    //     </div>
-    //     )
-    // });
 
 
     return (
       <div className="App">
-        Sandwich
+        <Chart weatherArray={this.state.loading ? true : weatherArray}/>
       </div>
     );
   }
