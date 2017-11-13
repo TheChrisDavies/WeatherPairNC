e3c1f304e4ebfc67076a7ef3d79f5ff552d5395c
Timbuckiii <30511045+Timbuckiii@users.noreply.github.com>
2017-11-13 17:26:55 -0500

    first functional comparison

--- weatherpairnc/src/App.js
+++ weatherpairnc/src/App.js
@@ -6,44 +6,91 @@ import recharts from 'recharts';
 
 //OpenWeatherMap API Key 04555c7b75822478f6403a14d3b3a458
 
-let weatherAPI = 'http://api.openweathermap.org/data/2.5/forecast?id=6167865&units=metric&APPID=04555c7b75822478f6403a14d3b3a458';
-let weatherJSX;
 let weatherArray;
+let weatherArray2;
+let city1;
+let city2;
 
 class App extends Component {
   constructor() {
     super();
     this.state = {
       weatherInfo: {list: []},
-      loading: true
+      weatherInfo2: {list: []},
+      loading: true,
+      loading2: true,
     }
-    // Example binding
-    // this.playSong = this.playSong.bind(this);
+
+  this.getWeather = this.getWeather.bind(this);
   }
 
-  componentWillMount(){
-    axios.get(weatherAPI)
+  getWeather(e){
+    e.preventDefault();
+    
+
+    
+    let city = document.getElementById('city').value.toLowerCase();
+    let country = document.getElementById('country').value.toLowerCase();
+
+    let city2 = document.getElementById('city2').value.toLowerCase();
+    let country2 = document.getElementById('country2').value.toLowerCase();
+
+    let weatherAPI = `http://api.openweathermap.org/data/2.5/forecast?q=${city},${country}&units=metric&APPID=410d0d04903511f9ddb027ae7b887c75`;
+    let weatherAPI2 = `http://api.openweathermap.org/data/2.5/forecast?q=${city2},${country2}&units=metric&APPID=410d0d04903511f9ddb027ae7b887c75`;
+
+    this.setState({
+      loading: true,
+      loading2: true,
+    })
+
+    Promise.all([axios.get(weatherAPI), axios.get(weatherAPI2)])
     .then(response => {
-      
+      let weatherInfo = response[0].data;
+      let weatherInfo2 = response[1].data;
+
+      console.log(weatherInfo)
+      console.log(weatherInfo2)
+
       this.setState({
-        weatherInfo: response.data,
-        loading: false
+        weatherInfo: weatherInfo,
+        weatherInfo2: weatherInfo2,
+        loading: false,
+        loading2: false,
       })
     })
     .catch(function (error) {
-      console.log(error);
+        console.log(error);
     });
   }
 
   render() {
 
-    if(this.state.loading === false){
+    if(this.state.loading === false && this.state.loading2 === false){
       weatherArray = Array.from(this.state.weatherInfo.list);
+      weatherArray2 = Array.from(this.state.weatherInfo2.list);
+      city1 = Object.assign({}, this.state.weatherInfo.city);
+      city2 = Object.assign({}, this.state.weatherInfo2.city);
     }
 
     return (
       <div className="App">
-        <Chart weatherArray={this.state.loading ? false : weatherArray}/>
+        <form onSubmit={(e) => {this.getWeather(e)}}>
+          <input type="text" name="city" id="city"  />
+          <select id="country">
+            <option value="ca">Canada</option>
+            <option value="us">US</option>
+          </select>
+          <input type="submit" value="Submit"/>
+        </form>
+        <form onSubmit={(e) => {this.getWeather(e)}}>
+          <input type="text" name="city" id="city2"  />
+          <select id="country2">
+            <option value="ca">Canada</option>
+            <option value="us">US</option>
+          </select>
+          <input type="submit" value="Submit"/>
+        </form>
+        <Chart weatherArray={this.state.loading ? false : weatherArray} city1={this.state.loading ? false : city1} weatherArray2={this.state.loading2 ? false : weatherArray2} city2={this.state.loading ? false : city2}/>
       </div>
     );
   }
--- weatherpairnc/src/Chart.js
+++ weatherpairnc/src/Chart.js
@@ -3,77 +3,50 @@ import './App.css';
 import {BarChart, Bar, ReferenceLine, XAxis, YAxis, CartesianGrid, Tooltip, Legend} from 'recharts';
 
 let weatherJSX;
-let temperatures = {}
+let weatherJSX2;
 
 class Chart extends Component {
 
-
-
-
     render(){
 
-        if (Array.isArray(this.props.weatherArray)) {
+        let city1 = this.props.city1["name"]
+        let city2 = this.props.city2["name"];
+
+        function Temp(temp, temp2, time) {
+            this[city1] = temp;
+            this[city2] = temp2;
+            this.time = time; 
+        }
+
+        if (Array.isArray(this.props.weatherArray) && Array.isArray(this.props.weatherArray2)) {
             let copy = this.props.weatherArray;
+            let copy2 = this.props.weatherArray2;
+
+            for (let i=0; i < copy.length; i++){
+                copy[i].main.temp2 = copy2[i].main.temp;
+            }
+
+            console.log(copy)
+
             weatherJSX = copy.map((individualTemp, index) => {
-                temperatures.push(individualTemp.main.temp)
-                //need to add time here
+                return new Temp(individualTemp.main.temp, individualTemp.main.temp2, individualTemp.dt_txt)
             })
-            createChart();
         }
-
-
-        function Temp(temp, date) {
-            this.temperature = temp;
-            // this.date = 
-        }
-
-        // var myFather = new person("John", "Doe", 50, "blue");
-        // var myMother = new person("Sally", "Rally", 48, "green");
-
-        function createChart(){
-            // const {BarChart, Bar, ReferenceLine, XAxis, YAxis, CartesianGrid, Tooltip, Legend} = Recharts;
-            const data = [
-                //   {name: 'Page A', uv: 4000, pv: 2400, amt: 2400},
-                //   {name: 'Page B', uv: -3000, pv: 1398, amt: 2210},
-                //   {name: 'Page C', uv: -2000, pv: -9800, amt: 2290},
-                //   {name: 'Page D', uv: 2780, pv: 3908, amt: 2000},
-                //   {name: 'Page E', uv: -1890, pv: 4800, amt: 2181},
-                //   {name: 'Page F', uv: 2390, pv: -3800, amt: 2500},
-                //   {name: 'Page G', uv: 3490, pv: 4300, amt: 2100},
-                weatherJSX
-            ];
-            // const SimpleBarChart = React.createClass({
-            //     render () {
-            //       return (
-
-            //     );
-            //   }
-            // })
-            
-            // ReactDOM.render(
-            //   <SimpleBarChart />,
-            //   document.getElementById('container')
-            // );
-        }
-
-
-        
-
 
         return(
             <div>
                 <div id="container">
-                <BarChart width={600} height={300} data={data}
-                        margin={{top: 5, right: 30, left: 20, bottom: 5}}>
-                   <XAxis dataKey="name"/>
-                   <YAxis/>
-                   <CartesianGrid strokeDasharray="3 3"/>
-                   <Tooltip/>
-                   <Legend />
-                   <ReferenceLine y={0} stroke='#000'/>
-                   <Bar dataKey="pv" fill="#8884d8" />
-                   <Bar dataKey="uv" fill="#82ca9d" />
-                  </BarChart>
+                    <BarChart width={1600} height={300} data={weatherJSX}
+                            margin={{top: 5, right: 30, left: 20, bottom: 5}}>
+                        <XAxis dataKey="time"/>
+                        <YAxis/>
+                        <CartesianGrid strokeDasharray="5 5"/>
+                        <Tooltip/>
+                        <Legend />
+                        <ReferenceLine y={0} stroke='#000'/>
+                        <Bar dataKey={city1} fill="#8884d8" />
+                        <Bar dataKey={city2} fill="#82ca9d" />
+                    </BarChart>
                 </div>
             </div>
         )
